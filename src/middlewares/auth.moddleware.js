2c521dd6a8a6205ffd3d8d95be5414215e8358a5
Eleusis Carretero <eleusis.carrtero@gmail.com>
2025-07-17 22:27:44 -0600

    feat: create auth middle weare

---
+++ src/middlewares/auth.moddleware.js
@@ -0,0 +1,19 @@
+import { JsonWebTokenError } from "jsonwebtoken";
+import { statusCodes } from "../controllers/codes";
+import { model } from "mongoose";
+const jwt = require('jsonwebtoken');
+
+function auth (req, res, next){
+    const token = req.params.token;
+    if(!token){
+        return res.status(statusCodes.clientError.badReq).json({message:"You need a token 我的朋友！🦾🦾"})
+    }
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    }catch(error){
+        return res.status(statusCodes.clientError.badReq).json({message:"You token has been expired! 😿🙀"})
+    }
+}
+module.exports = auth;
